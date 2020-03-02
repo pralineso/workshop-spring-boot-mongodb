c295ee94e0164d6179693bd95e51c4eac277a754
Praline Solutions <mxpixel@yahoo.com.br>
2020-03-02 18:22:14 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/pralineso/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pralineso/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.pralineso.workshopmongo.domain.Post;
 import com.pralineso.workshopmongo.domain.User;
+import com.pralineso.workshopmongo.dto.AuthorDTO;
 import com.pralineso.workshopmongo.repository.PostRepository;
 import com.pralineso.workshopmongo.repository.UserRepository;
 
@@ -35,11 +36,12 @@ public class Instantiation implements CommandLineRunner{
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/02/2020"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("22/02/2020"), "Bom Dia!", "Acordei muito feliz!", maria);
-		
+	
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/02/2020"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("22/02/2020"), "Bom Dia!", "Acordei muito feliz!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
